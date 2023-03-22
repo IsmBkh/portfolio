8dbcce1b41ede3d869ea3fd89a48f489d6c543a2
Ismail.B <i.bkh@hotmail.fr>
2023-03-22 20:22:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Ismail Bakhtaoui</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<!-- http://localhost/portfolio/index.php -->
+<body>
+    <header>
+        <nav class="navbar navbar-expand navbar-white container">
+            <a href="index.php" class="text-decoration-none nav-link btn">Ismaï Bakhtaoui</a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item p-2">
+                        <a href="index.php" class="nav-link btn btn-outline-secondary">Mes projets</a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a href="index.php?page=contact" class="nav-link btn btn-outline-secondary">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="hero">
+            <h1 class="p-2">Développeur Junior</h1>
+            <p>
+                <img src="assets/img/place.svg" alt="" width="20" height="20">
+                 Montfermeil
+            </p>
+            <a href="index.php?page=contact" class="btn btn-outline-light">Me contacter</a>
+        </div>
+
+    </main>
+    
+</body>
+</html>
