632fc7a44c426085ab73a1abb074063f5b6c5f74
Ismail.B <i.bkh@hotmail.fr>
2023-03-24 01:13:37 +0100

    creation de card en cours

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio Ismail Bakhtaoui</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 <!-- http://localhost/portfolio/index.php -->
 <body>
     <header>
         <nav class="navbar navbar-expand navbar-white container">
-            <a href="index.php" class="text-decoration-none nav-link btn">Ismaï Bakhtaoui</a>
+            <a href="index.php" class="text-decoration-none nav-link btn-light">Ismaï Bakhtaoui</a>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item p-2">
                         <a href="index.php" class="nav-link btn btn-outline-secondary">Mes projets</a>
@@ -34,6 +34,45 @@
             </p>
             <a href="index.php?page=contact" class="btn btn-outline-light">Me contacter</a>
         </div>
+
+        <section class="about">
+            <h2>à propos</h2>
+            <div class="info">
+                <img src="" alt="">
+                <div>
+                    <h3>Mon parcours</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eius ex dolorum ea</p>
+                    <h3>Soft skills</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+
+                    <a href="../assets/img/IsmailBakhtaoui-CVinformatique.pdf" download>Télécharger mon CV</a> 
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <h3>Ismaïl Bakhtaoui</h3>
+                    <q>
+                        "Ce que je préfère dans le métier de Développeur Web ?"
+                    </q>
+                    <br>
+                    <q>
+                        "Résoudre les problèmes, persévérer, créer."
+                    </q>
+                    <ul>
+                        <li>26 ans</li>
+                        <li>Clichy-sous-Bois</li>
+                        <li>Hobbies: Sports, cinéma, lecture, langues étrangères</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
 
     </main>
     
