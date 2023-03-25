fcf771dfd64e75995bffc44dee120c27ef556b09
Ismail.B <i.bkh@hotmail.fr>
2023-03-25 01:08:32 +0100

    main + nav okay

--- index.php
+++ index.php
@@ -7,18 +7,25 @@
     <title>Portfolio Ismail Bakhtaoui</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    
 </head>
+
 <!-- http://localhost/portfolio/index.php -->
+
 <body>
+
     <header>
+
+    
+
         <nav class="navbar navbar-expand navbar-white container">
-            <a href="index.php" class="text-decoration-none nav-link btn-light">Ismaï Bakhtaoui</a>
+            <a href="index.php" class="logo text-decoration-none nav-link btn-light">Ismaïl Bakhtaoui</a>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item p-2">
-                        <a href="index.php" class="nav-link btn btn-outline-secondary">Mes projets</a>
+                        <a href="index.php" class="nav-link">Mes projets</a>
                     </li>
                     <li class="nav-item p-2">
-                        <a href="index.php?page=contact" class="nav-link btn btn-outline-secondary">Contact</a>
+                        <a href="index.php?page=contact" class="nav-link">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -28,31 +35,40 @@
     <main>
         <div class="hero">
             <h1 class="p-2">Développeur Junior</h1>
-            <p>
+            <a href="https://goo.gl/maps/qD1xefVx436HsKXL8" target="_blank">
                 <img src="assets/img/place.svg" alt="" width="20" height="20">
-                 Montfermeil
-            </p>
+                <strong>Montfermeil</strong> 
+            </a>
             <a href="index.php?page=contact" class="btn btn-outline-light">Me contacter</a>
         </div>
 
+
+        <!-- A PROPOS -->
+
         <section class="about">
-            <h2>à propos</h2>
+
+                <h2>A propos</h2>
+
             <div class="info">
-                <img src="" alt="">
+
                 <div>
                     <h3>Mon parcours</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestias.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eius ex dolorum ea</p>
+
                     <h3>Soft skills</h3>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Esprit d'équipe</li>
+                        <li>Autonome</li>
+                        <li>Persévérant</li>
+                        <li>Ambitieux</li>
+                        <li>Ouverture d'esprit</li>
+                        <li>Intelligence émotionnelle</li>
+                        <li>Créatif</li>
+                        <li>Organisé</li>
                     </ul>
 
-                    <a href="../assets/img/IsmailBakhtaoui-CVinformatique.pdf" download>Télécharger mon CV</a> 
+                    <a href="../assets/img/IsmailBakhtaoui-CVinformatique.pdf" target="_self" download="CV Ismail BAKHTAOUI">Télécharger mon CV</a> 
                 </div>
 
                 <div>
@@ -60,18 +76,21 @@
                     <h3>Ismaïl Bakhtaoui</h3>
                     <q>
                         "Ce que je préfère dans le métier de Développeur Web ?"
-                    </q>
                     <br>
-                    <q>
                         "Résoudre les problèmes, persévérer, créer."
                     </q>
+
                     <ul>
                         <li>26 ans</li>
                         <li>Clichy-sous-Bois</li>
                         <li>Hobbies: Sports, cinéma, lecture, langues étrangères</li>
                     </ul>
+
                 </div>
+
             </div>
+
+
         </section>
 
     </main>
